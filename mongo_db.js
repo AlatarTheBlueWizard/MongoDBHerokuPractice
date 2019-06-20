3b2de1c9c8240e9f2a29841cf86f62a2c6d918cc
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-06-19 19:26:44 -0600

    Added connection

---
+++ mongo_db.js
@@ -0,0 +1,14 @@
+var mongodb = require('mongodb');
+
+var mongoClient = mongodb.MongoClient;
+
+var url = 'mongodb+srv://admin:admin@exdatabase-4y1xa.mongodb.net/test?retryWrites=true&w=majority';
+
+MongoClient.connect(url, function(err, db) {
+	if(err) {
+		console.log('Unable to connect to the mongoDB server. Error:', err);
+	} else {
+		console.log('Connection established to', url);
+		db.close();
+	}
+});
